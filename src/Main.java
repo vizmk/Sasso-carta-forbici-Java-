9f9693b638ac47d702b8549b215c970fb21cd6be
vincenzocavallaro <vincenzo.cavallaro98@gmail.com>
2026-01-16 23:46:34 +0100

    Fase 3-"Endless game"

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,25 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         Logic logic=new Logic();
         Scanner scanner=new Scanner(System.in);
-        //leggiamo la scelt
-        logic.test();
-        String chose= scanner.next();
+        boolean exit=false;
+        //start cicle
 
-        logic.chose(chose);
+        do{
+            logic.test();
+            String chose = scanner.next();
+            if((chose.equals("paper")||(chose.equals("rock"))||(chose.equals("scissors"))||(chose.equals("!exit")))) {
 
+                if (chose.equals("!exit")) {
+                    exit = true;
+                }
+                logic.chose(chose);
+            }else {
+                System.out.println("Invalid input");
+            }
+        }while(!exit);
 
 
     }
